2ec82f1ad716f639c0ed3a79047015c1b2c93b40
unknown <vicous@free.fr>
2024-01-24 19:05:46 +0200

    react + ws squeleton

--- blinkchat-back/index.js
+++ blinkchat-back/index.js
@@ -1,16 +1,144 @@
-import express from 'express'
-import {createServer} from 'node:http'
-import {Server} from 'socket.io'
+// Define the Message class
+class Message {
+    constructor(message, author, date) {
+      this.message = message;
+      this.author = author;
+      this.date = date;
+    }
+    
+  }
+  
+  // Define the Room class
+  class Room {
+    constructor(name, author) {
+      this.name = name;
+      this.author = author;
+      this.messages = [];
+    }
+  
+    addMessage(message) {
+      this.messages.push(message);
+    }
+    getName(){
+      return this.name;
+    }
+    getMessages(){
+      return this.messages;
+    }
+  
+  }
+  
+  function writeDb(rooms){
+   fs.writeFile('data/jsonDb.json', JSON.stringify(rooms), (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+    } else {
+      console.log('File written successfully!');
+    }
+  });
+  }
+  // function readDb(filePath) {
+  //   return new Promise((resolve, reject) => {
+  //     fs.readFile(filePath, 'utf8', (err, data) => {
+  //       if (err) {
+  //         reject(err);
+  //       } else {
+  //         resolve(data);
+  //       }
+  //     });
+  //   });
+  // }
+    
+//   const fs = require('fs');
+  import express from 'express';
+  import { createServer } from'node:http';
+  import { join } from 'node:path';
+  import { Server } from'socket.io';
+  import cors  from 'cors';
+  const app = express();
+  const httpServer = createServer(app);
+const io = new Server(httpServer, { 
+    
 
-const app = express()
-const server = createServer(app)
-const io = new Server(server)
+    cors: {
+        origin: "http://localhost:3001",
+        
+      }
+ });
 
+// io.on("connection", (socket) => {
+//   // ...
+// });
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: "hello world"})
-})
-
-server.listen(3000, () => {
-    console.log("Server running")
-})
+httpServer.listen(3000);
+  
+  app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'));
+  });
+  
+  let room = new Room('Cuisine', 'vicous');
+  let message1 = new Message('trop bon les haricots', 'vicous', '10/12/2048');
+  let message2 = new Message('trop bon les pates', 'vicous', '10/12/2048');
+  room.addMessage(message1);
+  room.addMessage(message2);
+  
+  let room2 = new Room('tropique', 'vicous');
+  let message3 = new Message('trop bon les citrons', 'vicous', '10/12/2048');
+  let message4 = new Message('trop bon les carbonaras', 'vicous', '10/12/2048');
+  room2.addMessage(message3);
+  room2.addMessage(message4);
+  
+  let rooms = [];
+  // console.log(readDb())
+  rooms.push(room);
+  rooms.push(room2);
+  
+  console.log(rooms);
+  io.on('connection', (socket) => {
+    console.log('a user connected');
+    // console.log(readDb())
+    let roomsName = []
+    for(let i = 0 ; i < rooms.length;i++){
+      roomsName.push(rooms[i].getName())
+    }
+    socket.emit('connected');
+    socket.emit('rooms', roomsName);
+  
+    // console.log(mongoData)
+    socket.on('create room', (roomName) => {
+      console.log("Je suis"+socket.id)
+      console.log("demande de creation de " + roomName )
+      // gerer lidentité avec des cookies ?
+    })
+    socket.on('delete room', (roomName) => {
+      console.log("demande de suppresion")
+      console.log(roomName)
+  
+  
+    })
+    socket.on('change room', (roomName) => {
+      // console.log(JSON.stringify(rooms));
+      // console.log(mongoData)
+      
+        let messages = []
+        for(let i = 0 ; i < rooms.length;i++){
+         
+          console.log(rooms[i].getName()=== roomName)
+          if(rooms[i].getName()=== roomName){
+            console.log(room)
+            socket.emit('display message', rooms[i]);
+        }
+        }
+        
+      
+    });
+  
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+  
+//   server.listen(3000, () => {
+//     console.log('server running at http://localhost:3000');
+//   });
+  
--- blinkchat-front/src/App.js
+++ blinkchat-front/src/App.js
@@ -1,26 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
 import Repository from "./data/Repository.js";
+import { io } from 'socket.io-client';
+import React, { useState, useEffect } from 'react';
+
+function ButtonList({salons, changeRoomFn}){
+  return (
+    salons.map((salon) => (<button key={salon} onClick={() => changeRoomFn(salon)}>{salon}</button>))
+  )
+}
+
+function Message({message}){
+  return (
+    <li>
+      <h1>{message.message} </h1>
+      <h2>{message.author}</h2>
+      <h3>{message.date}</h3>
+      <button>X</button>
+    </li>)
+}
 
 function App() {
-  let repository = new Repository()
+  let socket = io("http://localhost:3000");
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [salons, setSalons] = useState([]);
+  const [messages, setMessages] = useState([]);
+
+
+  function changeRoomFn (room){
+    socket.emit('change room', room)
+    socket.on('display message', (value)=>{
+      setMessages(() => value.messages)
+    })
+  }
+
+  useEffect(() => {
+    function onConnect() {
+      console.log("connecté au back")
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      console.log("deconnecté")
+      setIsConnected(false);
+    }
+
+    function onGetRooms(rooms){
+      console.log("bipboop je vais chercher les salons")
+      setSalons(() => rooms)
+    }
+
+    socket.on('connected', onConnect);
+    socket.on('disconnect', onDisconnect);
+    socket.on('rooms', onGetRooms)
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>{repository.getChannels()}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isConnected ? "connecté!" : "Non connecté"}
+      <ButtonList salons={salons} changeRoomFn={changeRoomFn}/>
+      <ul>
+        {messages.map((message)=> (
+          <Message message={message}/>
+        ))}
+      </ul>
     </div>
   );
 }
