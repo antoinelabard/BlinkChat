ea0f1e73ae6151e55681810eb17e2b9ec092ac92
Antoine Labard <antoine@labard.fr>
2024-01-25 17:16:36 +0100

    (db) update the schemas
    It is not possible to insert a document into another recursively (insert a message into a channel), without loading all the channel data, modify it and update it in the database.
    
    A relation between the 2 schemas has been implemented instead.

--- blinkchat-back/models/channel.js
+++ blinkchat-back/models/channel.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose"
 const channelSchema = mongoose.Schema({
     name: { type: String, required: true, unique: true },
     author: {type: String, required: true},
-    users: [{type: String}],
-    messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }],
+    users: [{type: String}]
 })
 
 const Channel = mongoose.model('Channel', channelSchema)
