1dc3901398ec0d0269c8056105b805b111ede2f6
Antoine Labard <antoine@labard.fr>
2024-01-25 10:59:29 +0100

    (db) add environment variables and successfully connect to the MongoDB database

--- blinkchat-back/index.js
+++ blinkchat-back/index.js
@@ -5,7 +5,16 @@ import {Server} from 'socket.io'
 const app = express()
 const server = createServer(app)
 const io = new Server(server)
+import mongoose from "mongoose"
+import 'dotenv/config'
 
+const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/?retryWrites=true&w=majority`
+mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connection to Mongoose successful'))
+    .catch(() => console.log('Connection to Mongoose failed'))
 
 app.get('/', (req, res) => {
     res.status(200).json({message: "hello world"})
