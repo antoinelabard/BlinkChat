bf463f3b3731782d2b98a7cffe97b262d4b5713c
Antoine Labard <antoine@labard.fr>
2024-01-25 17:36:03 +0100

    (db/Repository) add all the functions of the Repository with mock data as return values

--- blinkchat-back/data/Repository.js
+++ blinkchat-back/data/Repository.js
@@ -6,11 +6,18 @@ class Repository {
     getChannels() {
         Channel.find()
             .then(channels => {
-                console.log(channels)
+                return channels
             })
             .catch((error) => console.error(error))
+    }
 
-        return ["channel1", "channel2"]
+    getChannelByName(name) {
+        return new Channel({
+            "name": name,
+            "author": "author",
+            "users": ["Antoine", "Emeric", "Victor"],
+            "messages": []
+        })
     }
 
     addChannel(name, author) {
@@ -22,7 +29,6 @@ class Repository {
             "name": name,
             "author": author,
             "users": [],
-            "messages": []
         })
         channel.save()
             .then(() => {
@@ -49,15 +55,55 @@ class Repository {
                         })
                     default:
                         return new Message({
-                        "text": "An error occurred",
-                        "author": "System",
-                        "date": Date.now(),
-                        "recipient": author,
-                        "commandResult": "error"
-                    })
+                            "text": "An error occurred",
+                            "author": "System",
+                            "date": Date.now(),
+                            "recipient": author,
+                            "commandResult": "error"
+                        })
                 }
 
             })
+    }
+
+    renameChannel(channelName, newName, author) {
+        return new Message({
+            "text": `[NOT IMPLEMENTED YET] renameChannel: Success message`,
+            "author": "System",
+            "date": Date.now(),
+            "recipient": author,
+            "commandResult": "success"
+        })
+    }
+
+    deleteChannel(channelName, author) {
+        return new Message({
+            "text": `[NOT IMPLEMENTED YET] deleteChannel: Success message`,
+            "author": "System",
+            "date": Date.now(),
+            "recipient": author,
+            "commandResult": "success"
+        })
+    }
+
+    getMessagesByChannel(channelName) {
+        return [
+            new Message({
+                text: "Short message 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                author: "author1",
+                date: Date.now()
+            }),
+            new Message({
+                text: "Short message 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                author: "author2",
+                date: Date.now() - 10000 // now minus 10s
+            }),
+            new Message({
+                text: "Long message: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                author: "author1",
+                date: Date.now() - 20000 // now minus 10s
+            }),
+        ]
     }
 }
 
