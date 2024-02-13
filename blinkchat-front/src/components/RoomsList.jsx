a2820a462d1f550d335c6a88aa525c4b060a734b
Emeric Legendre <emeric.legendre@epitech.eu>
2024-02-13 11:50:46 +0100

    graphical delete room added

--- blinkchat-front/src/components/RoomsList.jsx
+++ blinkchat-front/src/components/RoomsList.jsx
@@ -1,3 +1,5 @@
+import deleteIcon  from "../assets/close-circle-outline.svg" ;
+
 export default function RoomsList({ rooms, graphicalJoinRoom, graphicalDeleteRoom}) {
   // soit une liste de :[username,salon]
   // soit une list d'objet message
@@ -7,7 +9,7 @@ export default function RoomsList({ rooms, graphicalJoinRoom, graphicalDeleteRoo
       <div>Liste des salons</div>
       <ul>
         {rooms.length ? (
-          rooms.map((elem) => <li key={elem.name} ><div onClick={() => graphicalJoinRoom(elem.name)} >{elem.name}</div><div onClick={() => graphicalDeleteRoom(elem.name)}>X</div></li>)
+          rooms.map((elem) => <li key={elem.name} ><div onClick={() => graphicalJoinRoom(elem.name)} >{elem.name}</div> <figure onClick={() => graphicalDeleteRoom(elem.name)}><img src={deleteIcon}></img></figure> </li>)
         ) : (
           <li>Pas de salon</li>
         )}
